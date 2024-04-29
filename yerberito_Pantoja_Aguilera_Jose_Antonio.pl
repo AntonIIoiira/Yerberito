efac3e3582659e115d563abad611a379581cac65
AntonIIoiira <98374431+AntonIIoiira@users.noreply.github.com>
2024-04-28 22:30:34 -0600

    Add files via upload

---
+++ yerberito_Pantoja_Aguilera_Jose_Antonio.pl
@@ -0,0 +1,144 @@
+
+
+% Propiedades medicinales que tienen las plantas
+propiedades(jengibre, antiinflamatorio).
+propiedades(jengibre, digestivo).
+propiedades(jengibre, estimulante).
+
+propiedades(menta, digestivo).
+propiedades(menta, relajante).
+propiedades(menta, alivio_dolor).
+
+propiedades(eucalipto, descongestionante).
+propiedades(eucalipto, expectorante).
+
+propiedades(calendula, cicatrizante).
+propiedades(calendula, antiinflamatorio).
+
+
+
+% Metodo de uso para las plantas
+metodo_de_uso(jengibre, infusion).
+metodo_de_uso(jengibre, cataplasma).
+metodo_de_uso(jengibre, decocción).
+
+metodo_de_uso(menta, compresa).
+metodo_de_uso(menta, infusion).
+metodo_de_uso(menta, aceite_esencial).
+
+
+metodo_de_uso(eucalipto, pomada).
+metodo_de_uso(eucalipto, inhalación).
+
+metodo_de_uso(calendula, ungüento).
+metodo_de_uso(calendula, compresa).
+
+
+% Relación entre plantas y para que se pueden tratar
+cura(jengibre, gripe).
+cura(jengibre, dolor_estomacal).
+cura(jengibre, nauseas).
+
+cura(menta, indigestión).
+cura(menta, dolor_de_cabeza).
+cura(menta, estrés).
+cura(menta, insomnio).
+
+cura(eucalipto, congestión_respiratoria).
+cura(eucalipto, tos).
+cura(eucalipto, sinusitis).
+
+cura(calendula, heridas).
+cura(calendula, inflamación).
+cura(calendula, irritación_piel).
+cura(calendula, quemaduras).
+
+% Fortalecimiento del organismo
+fortalece_sistema_digestivo(menta).
+fortalece_sistema_respiratorio(eucalipto).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------
+
+
+% Propiedades medicinales que tienen las plantas
+propiedades(malvavisco, laxante).
+propiedades(malvavisco, calmante).
+propiedades(malvavisco, diuretica).
+propiedades(malvavisco, expectorantes).
+
+
+propiedades(mangel, tinicas).
+propiedades(mangel, colagogas).
+propiedades(mangel, antidisentericas).
+
+
+
+
+propiedades(manzanilla, estimulante).
+propiedades(manzanilla, emenagogo).
+propiedades(manzanilla, carminativo).
+propiedades(manzanilla, estomaquico).
+
+
+
+
+propiedades(marrubio, febrifuga).
+propiedades(marrubio, diuretica).
+propiedades(marrubio, colagoga).
+propiedades(marrubio, pectoral).
+
+
+% Metodo de uso para las plantas
+metodo_de_uso(malvavisco, ungüento).
+metodo_de_uso(malvavisco, ingerir).
+
+metodo_de_uso(mangel, decocción).
+
+metodo_de_uso(manzanilla, decocción).
+
+metodo_de_uso(marrubio, decocción).
+
+
+
+% Relación entre plantas y para que se pueden tratar
+cura(malvavisco, reumatismo).
+cura(malvavisco, tos_catarral).
+cura(malvavisco, garganta_irritada).
+cura(malvavisco, insomnio).
+cura(malvavisco, etero_colitis).
+
+cura(mangel, enfermedades_de_vias_biliares).
+cura(mangel, extreñimiento).
+
+
+cura(manzanilla, diarrea).
+cura(manzanilla, irritacion_de_los_ojos).
+
+
+cura(marrubio, bronquios_inflamados).
+cura(marrubio, problemas_biliosos).
+cura(marrubio, congestion_del_higado).
+cura(marrubio, caida_del_pelo).
+
+
+% Fortalecimiento del organismo
+fortalece_sistema_digestivo(manzanilla).
+fortalece_sistema_digestivo(marrubio).
+
+
+
+
+
